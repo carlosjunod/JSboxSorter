e0587d5eaa48a8252fa693f09bd8951d46223ddc
carlosjunod <carlos@junod.cl>
2017-01-15 00:57:29 -0500

    adding fuction to sort boxes

--- js/boxSort.js
+++ js/boxSort.js
@@ -3,22 +3,47 @@ var min = 2;
 var max = 10;
 var boxes = [];
 
+console.log(max);
+
 for (var i = 0; i < radomizeBoxes(min, max); i++) {
-    var box = { width: radomizeBoxes(), height: radomizeBoxes(), color: '000'};
+    var box = { width: radomizeBoxes(), height: radomizeBoxes(), color: '000', id: i};
     boxes[i] = box;
 }
 
+// printing unsorted
+var unsortedContainer = document.querySelector('#unsorted');
+unsortedContainer.insertAdjacentHTML('beforeend', printer());
+
+// Sorting Boxes
+function sortBoxes(){
+
+    boxes.sort(function(a, b) {
+      return a.width - b.width;
+    });
+
+    // printing sorted
+    var sortedContainer = document.querySelector('#sorted');
+    sortedContainer.insertAdjacentHTML('beforeend', printer());
+
+    //disable button
+    document.querySelector('#organize').disabled = true;
+
+}
+
+// Helpers
 function radomizeBoxes(_min, _max){
     _min = _min || 50;
-    _max = _max || 500;
+    _max = _max || 600;
     return  Math.random() * (_max - _min) + _min;
 }
 
-var toPrint = '';
-boxes.forEach(function(b){
-    toPrint += '<div class="box" style="width:'+b.width+'px; height:'+b.height+'px; background-color:#'+b.color+'; border:1px solid #fff"></div>';
-    console.log(toPrint);
-});
+// to print on the screen
+function printer(){
 
-var unsortedContainer = document.querySelector('#unsorted');
-unsortedContainer.insertAdjacentHTML('beforeend', toPrint);
+    var toPrint = '';
+    boxes.forEach(function(b){
+        toPrint += '<div class="box" style="width:'+b.width+'px; height:'+b.height+'px; background-color:#'+b.color+'; border:1px solid #fff">'+b.id+'</div>';
+        //console.log(toPrint);
+    });
+    return toPrint;
+}
